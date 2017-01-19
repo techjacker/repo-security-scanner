1eeb4b0e5b196cdb2d82a1d68b189f67480b23e5
Andrew Griffiths <mail@andrewgriffithsonline.com>
2017-01-19 10:48:50 +0000

    Adds integration ID as environment variable to pass in to server

--- index.js
+++ index.js
@@ -2,18 +2,23 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
-const cert = process.env.GITHUB_PRIVATEKEY
+const GITHUB_PRIVATEKEY = process.env.GITHUB_PRIVATEKEY
 const GitHubApi = require('github')
 const PORT = process.env.PORT || 8080 // eslint-disable-line no-magic-numbers, no-process-env
+const INTEGRATION_ID = process.env.INTEGRATION_ID
+
+if (!GITHUB_PRIVATEKEY || !INTEGRATION_ID) {
+  throw new Error('missing needed environment variables INTEGRATION_ID or GITHUB_PRIVATEKEY')
+}
 
 app.use(bodyParser.json())
 
 app.post('/github', (req, res) => {
   if (req.headers['x-github-event'] !== 'push') {
-    return res.send('not for us')
+    return res.send('not a push event so ignoring')
   }
 
-  const token = jwt.sign({iss: 1076}, cert, {algorithm: 'RS256', expiresIn: '2 minutes'})
+  const token = jwt.sign({iss: INTEGRATION_ID}, GITHUB_PRIVATEKEY, {algorithm: 'RS256', expiresIn: '2 minutes'})
 
   const github = new GitHubApi({
     Promise: require('bluebird'),
